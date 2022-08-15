d04304a5387ed7cbd8f0eaa49cd1d42f55fe896a
MandlaHlaoli <86470130+MandlaHlaoli@users.noreply.github.com>
2022-08-15 22:08:02 +0200

    initial c

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+(function init () {
+  const APP_ID = 'AB0709D9-45C6-6063-FFC1-E7B759B99B00';
+  const API_KEY = '21EE0CF4-1C6D-4EA0-A47B-ACE0E1E2CC08';
+
+  Backendless.serverURL = 'https://api.backendless.com';
+  Backendless.initApp(APP_ID, API_KEY);
+
+  
+})();
+                
